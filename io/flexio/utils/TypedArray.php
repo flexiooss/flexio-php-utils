5a9aebd539721e755f803df1eb25fa88b373450a
nicolas@flexio.fr <nicolas@flexio.fr>
2018-07-23 12:26:49 +0200

    change class in validation function

--- io/flexio/utils/TypedArray.php
+++ io/flexio/utils/TypedArray.php
@@ -8,31 +8,26 @@ use \Exception;
 class TypedArray extends ArrayObject {
    
     private $clazz;
-   
-    public function __construct( $clazz, $input = array() ){
-        if( !class_exists( $clazz ) ){
-            throw new Exception( 'Invalid class: the specified class is not found' );
+    private $validate;
+
+    public function __construct( $validate, $input = array() ){
+        if( !is_callable( $validate ) ){
+            throw new Exception( 'The specified validation function is not callable' );
         }
+        $this->validate = $validate;
         $this->clazz = $clazz;
         foreach( $input as $item ){
-            if(! $item instanceof $clazz ){
-                throw new Exception( 'Invalid class: found an item in the initial array that is not an instance of the specified class' );
-            }else{
-                $this->offsetSet( null, $item );
-            }
+            $this->offsetSet( null, $item );
         }
     }
    
     public function append ( $value ){
-        $this->offsetSet( null, $value );
+        $this->offsetSet( null, $this->validate($value) );
     }
    
     public function offsetSet ( $index , $newval ){
-        if( $newval instanceof $this->clazz ){
-            parent::offsetSet( $this->count(), $newval );
-        }
-        else{
-            throw new Exception( 'Invalid class: inserted object is not an instance of expected class' );
-        }
+        $this->validate($item);
+        parent::offsetSet( $this->count(), $newval );
     }
+
 }
