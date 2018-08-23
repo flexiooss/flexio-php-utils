897abde107f2eae047aeb118208b48e2a17a2d26
nicolas@flexio.fr <nicolas@flexio.fr>
2018-08-23 10:35:17 +0200

    .

--- io/flexio/utils/http/HttpRequester.php
+++ io/flexio/utils/http/HttpRequester.php
@@ -11,9 +11,11 @@ interface HttpRequester {
     public function delete(): ResponseDelegate;
     public function head(): ResponseDelegate;
 
-    public function parameter( string $name, array $value ): HttpRequester;
+    public function parameter( string $name, string $value ): HttpRequester;
+    public function arrayParameter( string $name, string $value ): HttpRequester;
 
-    public function header( string $name, array $value ): HttpRequester;
+    public function header( string $name, string $value ): HttpRequester;
+    public function arrayHeader( string $name, array $value ): HttpRequester;
 
     public function path( string $path ): HttpRequester;
 }
