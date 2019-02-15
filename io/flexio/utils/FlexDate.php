1756e53ecb034645c732f006170c7601af743ede
tomacht <thomas@flexio.fr>
2019-02-15 12:34:58 +0100

    update flexdate

--- io/flexio/utils/FlexDate.php
+++ io/flexio/utils/FlexDate.php
@@ -5,7 +5,8 @@ namespace io\flexio\utils;
 use \DateTime;
 use \JsonSerializable;
 
-class FlexDate extends DateTime implements JsonSerializable {
+class FlexDate extends DateTime implements JsonSerializable
+{
 
     private $format;
 
@@ -19,29 +20,35 @@ class FlexDate extends DateTime implements JsonSerializable {
     const datetimeFormat = "Y-m-d\TG:i:s\Z";
     const zonedDatetimeFormat = "Y-m-d\TG:i:sP";
 
-    public function __construct( string $format, string $time ) {
-        parent::__construct( $time );
+    public function __construct(string $format, string $time = "now")
+    {
+        parent::__construct($time);
         $this->format = $format;
     }
 
-    public static function newTime( string $time ) {
-        return new FlexDate( FlexDate::timeFormat, $time );
+    public static function newTime(string $time = "now")
+    {
+        return new FlexDate(FlexDate::timeFormat, $time = "now");
     }
 
-    public static function newDate( string $time ) {
-        return new FlexDate( FlexDate::dateFormat, $time );
+    public static function newDate(string $time)
+    {
+        return new FlexDate(FlexDate::dateFormat, $time = "now");
     }
 
-    public static function newDateTime( string $time ) {
-        return new FlexDate( FlexDate::datetimeFormat, $time );
+    public static function newDateTime(string $time)
+    {
+        return new FlexDate(FlexDate::datetimeFormat, $time = "now");
     }
 
-    public static function newTzDateTime( string $time ) {
-        return new FlexDate( FlexDate::zonedDatetimeFormat, $time );
+    public static function newTzDateTime(string $time)
+    {
+        return new FlexDate(FlexDate::zonedDatetimeFormat, $time = "now");
     }
 
-    public function jsonSerialize() {
-        return $this->format( $this->format );
+    public function jsonSerialize()
+    {
+        return $this->format($this->format);
     }
 
     /**
@@ -49,17 +56,18 @@ class FlexDate extends DateTime implements JsonSerializable {
      * @return FlexDate
      * @throws \Exception
      */
-    public static function parse( $date ) {
-        if( preg_match( FlexDate::timePattern, $date ) ) {
-            return FlexDate::newTime( $date );
-        } else if( preg_match( FlexDate::datePattern, $date ) ) {
-            return FlexDate::newDate( $date );
-        } else if( preg_match( FlexDate::datetimePattern, $date ) ) {
-            return FlexDate::newDateTime( $date );
-        } else if( preg_match( FlexDate::zonedDatetimePattern, $date ) ) {
-            return FlexDate::newTzDateTime( $date );
+    public static function parse($date)
+    {
+        if (preg_match(FlexDate::timePattern, $date)) {
+            return FlexDate::newTime($date);
+        } else if (preg_match(FlexDate::datePattern, $date)) {
+            return FlexDate::newDate($date);
+        } else if (preg_match(FlexDate::datetimePattern, $date)) {
+            return FlexDate::newDateTime($date);
+        } else if (preg_match(FlexDate::zonedDatetimePattern, $date)) {
+            return FlexDate::newTzDateTime($date);
         }
-        throw new \Exception( "Unparsable date" ); // #TODO: tz date not implemented yet
+        throw new \Exception("Unparsable date"); // #TODO: tz date not implemented yet
     }
 
 }
